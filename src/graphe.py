3d5bb6ed6c235445b377dc9b8f62acc8cda689f3
ROMAIN REN <caelumcis@gmail.com>
2024-11-06 21:54:03 +0100

    Ajout algo Bellman-Ford

--- src/graphe.py
+++ src/graphe.py
@@ -38,12 +38,47 @@ for i in range(len(aretes)):
 # Fermeture du fichier metro.txt
 file.close()
 
-# Création et paramètrage de la fenêtre principale
-fenetre = tk.Tk()
-fenetre.title("Metro Parisien")
-fenetre.geometry("800x800")
+def bellman_ford(graph, debut, fin):
+    # Initialise les distances
+    distances = {noeud: float('inf') for noeud in graph}
+    distances[debut] = 0  # Distance to the debut vertex is 0
+    predecessors = {noeud: None for noeud in graph}
 
-label = tk.Label(fenetre, text="Hello World")
-label.pack()
+    for _ in range(len(graph) - 1):
+        for noeud in graph:
+            for voisin, poids in graph[noeud]:
+                if distances[noeud] + poids < distances[voisin]:
+                    distances[voisin] = distances[noeud] + poids
+                    predecessors[voisin] = noeud
 
-fenetre.mainloop()
+    # Vérifie s'il y a des valeurs négatifs (Bellman-Ford)
+    for noeud in graph:
+        for voisin, poids in graph[noeud]:
+            if distances[noeud] + poids < distances[voisin]:
+                raise ValueError("Il y a une valeur négatif")
+
+    # Construit le chemin le plus court
+    chemin = []
+    actuel = fin
+    while actuel is not None:
+        chemin.insert(0, actuel)
+        actuel = predecessors[actuel]
+
+    if distances[fin] == float('inf'):
+        return None, float('inf')
+
+    for i in range(len(chemin)):
+        print(stations[chemin[i]][1], "" , chemin[i])
+    return chemin, distances[fin]
+
+# Example usage
+debut_station = 48  # Replace with your debuting station
+end_station = 365  # Replace with your destination station
+
+chemin, distance = bellman_ford(graphe, debut_station, end_station)
+
+"""print("Voici le chemin : ")
+print(chemin)
+print("Voici la distance : ")
+print(distance)"""
+file.close()
